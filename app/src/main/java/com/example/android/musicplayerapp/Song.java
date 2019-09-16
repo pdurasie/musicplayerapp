61f1a852a5eb93d6b120d988b620bc03fd972609
Patrick Durasiewicz <pdurasie@pm.me>
2019-09-16 10:23:56 +0200

    feat: Create Song class

---
+++ app/src/main/java/com/example/android/musicplayerapp/Song.java
@@ -0,0 +1,33 @@
+package com.example.android.musicplayerapp;
+
+public class Song {
+
+    private String mSongName;
+    private String mArtistName;
+    private int mImageResourceId;
+    private boolean mIsHearted;
+
+    public Song(String songName, String artistName, int imageResourceId, boolean isHearted) {
+        mSongName = songName;
+        mArtistName = artistName;
+        mImageResourceId = imageResourceId;
+        mIsHearted = isHearted;
+    }
+
+    public String getSongName() {
+        return mSongName;
+    }
+    
+    public String getArtistName() {
+        return mArtistName;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean getIsHearted() {
+        return mIsHearted;
+    }
+
+}
