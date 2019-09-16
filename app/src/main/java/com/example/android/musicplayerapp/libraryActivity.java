3ed862e7261593a3ff6c86233f774e240951ad9b
Patrick Durasiewicz <pdurasie@pm.me>
2019-09-16 10:03:24 +0200

    feat: Create other activity screens

---
+++ app/src/main/java/com/example/android/musicplayerapp/libraryActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.musicplayerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class libraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
